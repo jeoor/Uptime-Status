127134c8ee9fcd167f1f466dfc493f1cdffd7638
Kayro <2958975137@qq.com>
2026-03-01 19:03:06 +0800

    Fix formatting in Footer.vue component

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -85,14 +85,15 @@
           Copyright ©
           <span v-if="currentYear > 2026">2026 - {{ currentYear }}</span>
           <span v-else>2026</span>
-          <a 
+          <a
             :href="pkg.url"
             target="_blank"
             rel="noopener noreferrer"
             class="font-semibold hover:text-emerald-500 dark:hover:text-emerald-400 transition-colors"
           >
-          敖苛
+            敖苛
           </a>
+        </div>
       </div>
     </div>
   </footer>
